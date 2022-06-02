6bc17d196778062854d60624a67ae9ec115f32f3
georgemwangi118 <gmwangi118@yahoo.com>
2022-06-02 17:19:14 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Displays "Programming is like a building a multilingual puzzle
+ *
+ * Return: returns the value 0 if exited properly, otherwise non-zero
+ */
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0); /*can be ommited*/
+}
