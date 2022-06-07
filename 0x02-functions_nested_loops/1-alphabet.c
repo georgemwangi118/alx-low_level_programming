a4fc960eaa15ce70d8a91a52a3f90ace633311e0
georgemwangi118 <gmwangi118@yahoo.com>
2022-06-07 07:25:12 +0000

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * Print_alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
