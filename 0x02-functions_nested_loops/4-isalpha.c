018e00076533330573e29577258b4a253c050ac7
georgemwangi118 <gmwangi118@yahoo.com>
2022-06-07 07:43:29 +0000

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ *
+ * Return 1 if c is lower/upper, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
