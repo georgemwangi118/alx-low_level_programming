d1ef0da668fb43fb91b20c37f35317fcb3cde5c8
georgemwangi118 <gmwangi118@yahoo.com>
2022-06-21 12:59:02 +0000

    updated

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,18 +12,18 @@
 
 char *_strchr(char *s, char c)
 {
-	int a;
+		int a;
 
-	while (i)
-	{
-		a = *s++;
-		if (a == c)
+		while (i)
 		{
-			return (s - 1);
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
 		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
-	}
 }
